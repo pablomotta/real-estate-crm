b636b937c26a1fb912497200befa17cc40dc4763
fadiima92 <andrewfmadain@gmail.com>
2019-10-31 22:30:46 -0700

    .vscode settings folder

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,18 @@
-import React, { useContext, useState, useEffect } from 'react';
-import AlertContext from '../../context/alert/alertContext';
-import AuthContext from '../../context/auth/authContext';
+import React, { useContext, useState, useEffect } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBBtn,
+    MDBIcon,
+    MDBModalFooter,
+    MDBInputNumeric
+} from "mdbreact";
 
 const Register = props => {
     const alertContext = useContext(AlertContext);
@@ -11,33 +23,33 @@ const Register = props => {
 
     useEffect(() => {
         if (isAuthenticated) {
-            props.history.push('/');
+            props.history.push("/");
         }
 
-        if (error === 'User already exists') {
-            setAlert(error, 'danger');
+        if (error === "User already exists") {
+            setAlert(error, "danger");
             clearErrors();
         }
         // eslint-disable-next-line
     }, [error, isAuthenticated, props.history]);
 
     const [user, setUser] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
     });
 
-    const { name, email, password, password2 } = user;
+    const { name, name2, email, password, password2 } = user;
 
     const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
     const onSubmit = e => {
         e.preventDefault();
-        if (name === '' || email === '' || password === '') {
-            setAlert('Please enter all fields', 'danger');
+        if (name === "" || email === "" || password === "") {
+            setAlert("Please enter all fields", "danger");
         } else if (password !== password2) {
-            setAlert('Passwords do no match', 'danger');
+            setAlert("Passwords do no match", "danger");
         } else {
             register({
                 name,
@@ -48,60 +60,116 @@ const Register = props => {
     };
 
     return (
-        <div className='form-container'>
-            <h1>
-                Account <span className='text-primary'>Register</span>
-            </h1>
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <label htmlFor='name'>Name</label>
-                    <input
-                        type='text'
-                        name='name'
-                        value={name}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='email'>Email Address</label>
-                    <input
-                        type='email'
-                        name='email'
-                        value={email}
-                        onChange={onChange}
-                        required
-                    />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        type='password'
-                        name='password'
-                        value={password}
-                        onChange={onChange}
-                        required
-                        minLength='6'
-                    />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password2'>Confirm Password</label>
-                    <input
-                        type='password'
-                        name='password2'
-                        value={password2}
-                        onChange={onChange}
-                        required
-                        minLength='6'
-                    />
-                </div>
-                <input
-                    type='submit'
-                    value='Register'
-                    className='btn btn-primary btn-block'
-                />
-            </form>
-        </div>
+        <MDBContainer>
+            <MDBRow>
+                <MDBCol md="6">
+                    <MDBCard>
+                        <MDBCardBody className="mx-4">
+                            <div className="text-center" onSubmit={onSubmit}>
+                                <h3 className="dark-grey-text mb-5">
+                                    <strong>Sign Up</strong>
+                                </h3>
+                            </div>
+                            <MDBInput
+                                label="First Name"
+                                type="text"
+                                value={name}
+                                onChange={onChange}
+                                containerClass="mb-0"
+                            />
+                            <MDBInput
+                                label="Last Name"
+                                type="text"
+                                value={name2}
+                                onChange={onChange}
+                                containerClass="mb-0"
+                            />
+                            <MDBInput
+                                label="Your email"
+                                type="text"
+                                value={email}
+                                onChange={onChange}
+                                error="wrong"
+                                success="right"
+                            />
+                            <MDBInput
+                                label="Your password"
+                                type="password"
+                                value={password}
+                                onChange={onChange}
+                                containerClass="mb-0"
+                            />
+                            <MDBInput
+                                label="Your password"
+                                type="password"
+                                value={password2}
+                                onChange={onChange}
+                                containerClass="mb-0"
+                            />
+                            <div className="text-center mb-3">
+                                <MDBBtn
+                                    type="button"
+                                    gradient="blue"
+                                    rounded
+                                    className="btn-block z-depth-1a"
+                                >
+                                    Create Account
+                                </MDBBtn>
+                            </div>
+                            <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
+                                or Sign in with:
+                            </p>
+                            <div className="row my-3 d-flex justify-content-center">
+                                <MDBBtn
+                                    type="button"
+                                    color="white"
+                                    rounded
+                                    className="z-depth-1a"
+                                >
+                                    <MDBIcon
+                                        fab
+                                        icon="facebook-f"
+                                        className="blue-text text-center"
+                                    />
+                                </MDBBtn>
+                                <MDBBtn
+                                    type="button"
+                                    color="white"
+                                    rounded
+                                    className="mr-md-3 z-depth-1a"
+                                >
+                                    <MDBIcon
+                                        fab
+                                        icon="twitter"
+                                        className="blue-text"
+                                    />
+                                </MDBBtn>
+                                <MDBBtn
+                                    type="button"
+                                    color="white"
+                                    rounded
+                                    className="z-depth-1a"
+                                >
+                                    <MDBIcon
+                                        fab
+                                        icon="google-plus-g"
+                                        className="blue-text"
+                                    />
+                                </MDBBtn>
+                            </div>
+                        </MDBCardBody>
+                        <MDBModalFooter className="mx-5 pt-3 mb-1">
+                            <p className="font-small grey-text d-flex justify-content-end">
+                                Not a member?
+                                <a href="#!" className="blue-text ml-1">
+                                    Sign Up
+                                </a>
+                            </p>
+                        </MDBModalFooter>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
